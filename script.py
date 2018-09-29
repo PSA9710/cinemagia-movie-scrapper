cdb7d270a412902f515ddea8382dcd8379e8b3ba
Stefan Precup <psa9710@gmail.com>
2018-09-29 16:01:08 +0300

    format and print movies

--- script.py
+++ script.py
@@ -4,7 +4,7 @@ import re
 from colorama import init
 from colored import fore, back, style
 
-init() #calls colorama's init so that colored works on windows
+init()  # calls colorama's init so that colored works on windows
 
 
 class color:
@@ -25,6 +25,13 @@ class Cinema(object):
         self.location = location
         self.hours = hours
 
+    def __str__(self):
+        format_location = fore.INDIAN_RED_1A+'\t\t'+self.location+":"+style.RESET
+        cinema_string = format_location
+        for hour in self.hours:
+            cinema_string += '\n\t\t\t' + str(hour)
+        return cinema_string
+
 
 class Hour(object):
     def __init__(self, hour, _3D=False, dubbed=False):
@@ -32,18 +39,40 @@ class Hour(object):
         self._3D = _3D
         self.dubbed = dubbed
 
+    def __str__(self):
+        format_hour = str(self.hour)
+        hour_string = format_hour
+        if self._3D:
+            hour_string += " 3D"
+        if self.dubbed:
+            hour_string += ' dubbed'
+        return hour_string
+
 
 class Movie(object):
 
-    def __init__(self, id, title, rating, genres, schedule):
+    def __init__(self, id, title, rating, genres, schedules):
         self.id = id
         self.title = title
         self.rating = rating
         self.genres = genres
-        self.schedule = schedule
+        self.schedules = schedules
 
     def __str__(self):
-        return self.title
+        format_title = '\t'+fore.TURQUOISE_2 + style.BOLD + self.title+style.RESET
+        format_rating = '\n\t\tIMDB:'
+        if float(self.rating) > 7.9:
+            format_rating += fore.LIGHT_GREEN
+        elif float(self.rating) > 4.9:
+            format_rating += fore.GOLD_1
+        else:
+            format_rating += fore.RED_3B
+        format_rating += self.rating+style.RESET
+        format_genres = '\n\t\tGenres: '+','.join(self.genres)
+        movie_string = format_title + format_rating + format_genres
+        for schedule in self.schedules:
+            movie_string += '\n'+str(schedule)
+        return movie_string+'\n'
 
 
 def get_location():
@@ -62,7 +91,7 @@ def get_location():
 
 def generate_url():
     url = "https://www.cinemagia.ro/program-cinema/"
-    return url+get_location()+'/sambata'
+    return url+get_location()+'/duminica'
 
 
 def get_cinemagia_page():
@@ -101,23 +130,27 @@ def get_movies():
                     _3D = True
                 if 'ro' in text:
                     dubbed = True
-                hours.append(Hour(text[:4], _3D, dubbed))
+                hours.append(Hour(text[:5], _3D, dubbed))
             schedule.append(Cinema(location, hours))
-        movie_obj = Movie(id, title, rating, genre, schedule)
+        movie_obj = Movie(id, title, rating, genres, schedule)
         movies_list.append(movie_obj)
 
         # manipulate movies to show the movie
     return movies_list
 
 
+def print_start_message():
+    print(fore.ORANGE_3+style.UNDERLINED+style.BOLD +
+          'Showing all movies that run on Sunday:\n'+style.RESET)
+
+
 def display_movies():
     movies = get_movies()
-    print()
+    print_start_message()
     for movie in movies:
         print(movie)
 
 
 display_movies()
-
-print()
+# print('\tAN')
 # print(color.BOLD +color.GREEN+ 'Hello World !' + color.END)
